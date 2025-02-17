db911d1dcc54822c21a4f8ef55ac1a8c50365b90
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-02-17 20:49:09 +0700

    custom hamburger button complete

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const navIcon = document.getElementById('nav-icon3')
-  const navbarNav = document.getElementById('navbarNav')
-
-  navIcon.addEventListener('click', function () {
-    navIcon.classList.toggle('open') // Animate hamburger
-    navbarNav.classList.toggle('show') // Show/hide Bootstrap menu
-  })
-})
